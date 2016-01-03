7a1bd84ec83c269bddff75b22cef51abacacb1ee
anwarrsyd <anwarrosyidi.5@gmail.com>
2016-01-03 16:03:02 +0700

    server select sama thread udah siap, bisa dilanjutin ya ke fungsi FTP, sumbernya dari gistnya pak Hudan

--- FTPServer.py
+++ FTPServer.py
@@ -3,11 +3,15 @@ import os
 import threading
 import select
 import sys
+import os
+import time
 
+currdir=os.path.abspath('.')
+local_ip='localhost'
 class Server:
 	def __init__(self):
-		self.host = 
-		self.port = 
+		self.host = localhost
+		self.port = 5000
 		self.size = 1024
 		self.server = None
 		self.threads = []
@@ -18,7 +22,43 @@ class Server:
         self.server.bind((self.host,self.port))
         self.server.listen(5) 
 
-    def run():
+    def run(self):
+    	self.open_socket()
+        input = [self.server, sys.stdin]
+        running = 1
+        while running:
+            inputready,outputready,exceptready = select.select(input,[],[])
+
+            for s in inputready:
+
+                if s == self.server:
+                    # handle the server socket
+                    c = Client(self.server.accept())
+                    c.start()
+                    self.threads.append(c)
+
+                elif s == sys.stdin:
+                    # handle standard input
+                    junk = sys.stdin.readline()
+                    running = 0
+
+        # close all threads
+
+        self.server.close()
+        for c in self.threads:
+            c.join()
+
+        class Client(threading.Thread):
+		    def __init__(self,(conn,addr)):
+		        self.conn=conn
+		        self.addr=addr
+		        self.basewd=currdir
+		        self.cwd=self.basewd
+		        self.rest=False
+		        self.pasv_mode=False
+		        self.passwd=''
+		        threading.Thread.__init__(self)
+
     	
 
 if __name__ == '__main__':
